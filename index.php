706d401e6ccd3d24004256ec636efd213663a15a
João Enrique Barbosa Santos Alves <99426704+JoaoEnrique13@users.noreply.github.com>
2022-06-23 11:05:15 -0300

    Update index.php

--- index.php
+++ index.php
@@ -23,7 +23,7 @@
         </style>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     </head>
-    <body>
+    <body style="display: flex!important;  justify-content: center; min-height: 100vh; align-items: center;  padding: 0 400px!important;">
     
         <div class="container">
             <div class="row">
